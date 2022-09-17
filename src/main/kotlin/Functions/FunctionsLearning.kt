35db002a4dd9d76b85ee1e3fb2dbd38ca7d90dd3
Ferdi <ferdibozkurt74@gmail.com>
2022-09-17 06:21:53 +0300

    Commit3

--- src/main/kotlin/Functions/FunctionsLearning.kt
+++ src/main/kotlin/Functions/FunctionsLearning.kt
@@ -1,15 +1,27 @@
 package Functions
 
 import java.util.InputMismatchException
+import java.util.StringJoiner
 
 fun main() {
     val number= takeCube(3)
-    println(number)
+    val car= cars("BMW","bissan")
+    getUserInfo("Ferdi","Bozkurt","25","", key = 4)
 }
 fun takeCube(number:Int):Int{
     fun takeSquare(number: Int):Int{
         return number*number
     }
+    println(number)
+
     //dsada
     return takeSquare(number)*number
+}
+fun cars(car:String,bike:String):String{
+    println("$car,$bike")
+    return car+bike
+}
+fun getUserInfo(vararg userInfo:String,key:Int){
+    userInfo[3]
+    userInfo.get(3)
 }
